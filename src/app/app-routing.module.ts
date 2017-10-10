0922dfab2348c39eb08a92c6210755b79748c97a
Stijn Jonckheere <stijnjonckheere@Stijns-MacBook-Pro-2.local>
2017-10-10 12:43:46 +0200

    player notes enabled

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,10 @@ import { AuthGuard } from "./auth/auth-guard.service";
 import { CharacterNewComponent } from "./character/character-new/character-new.component";
 import { CharacterEditComponent } from "./character/character-edit/character-edit.component";
 import { CharacterListComponent } from "./character/character-list/character-list.component";
+import { PlayerNotesComponent } from "./infrastructure/player-notes/player-notes.component";
 
 const appRoutes: Routes = [
+  { path: "player-notes", component: PlayerNotesComponent, canActivate: [AuthGuard] },
   { path: "", redirectTo: "characters", pathMatch: "full" },
   // { path: "**", redirectTo: "characters" }
 ];
---
+++ src/app/player-notes.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from "@angular/core";
+import { AuthService } from "./auth/auth.service";
+import { ErrorService } from "./error-service.service";
+import { Http } from "@angular/http";
+
+@Injectable()
+export class PlayerNotesService {
+
+  playerNotes: string;
+  notesFetched = false;
+
+  constructor(private authService: AuthService,
+    private errorService: ErrorService,
+    private http: Http) { }
+
+  fetchAdminNotes() {
+    const fetchPromise = new Promise(
+      (resolve, reject) => {
+        const userId = this.authService.getUserId();
+        const token = this.authService.getToken();
+        const url = "https://digital-dungeon-master.firebaseio.com/playernotes/" + userId + "-notes.json?auth=" + token;
+
+        this.http.get(url).subscribe(
+          (response) => {
+            const notes = response.json();
+            if (notes !== null) {
+              this.playerNotes = notes;
+            }
+            this.notesFetched = true;
+            resolve();
+          },
+          (error) => {
+            this.errorService.displayError(error.json().error);
+            reject(error);
+          }
+        );
+      }
+    );
+    return fetchPromise;
+  }
+
+  getPlayerNotes() {
+    const promise = new Promise(
+      (resolve, reject) => {
+        if (this.notesFetched) {
+          resolve(this.playerNotes);
+        } else {
+          this.fetchAdminNotes().then(
+            () => {
+              resolve(this.playerNotes);
+            }
+          );
+        }
+      }
+    );
+
+    return promise;
+  }
+
+  saveNotes(notes: string) {
+    const userId = this.authService.getUserId();
+    const token = this.authService.getToken();
+    const url = "https://digital-dungeon-master.firebaseio.com/playernotes/" + userId + "-notes.json?auth=" + token;
+    this.playerNotes = notes;
+
+    this.http.put(url, [notes]).subscribe(
+      (response) => { console.log("Player notes saved succesfully!"); },
+      (error) => {
+        this.errorService.displayError("Saving player notes failed! => " + error);
+      }
+    );
+  }
+
+}
